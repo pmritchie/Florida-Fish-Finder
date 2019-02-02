31509cca44cad4467d8207d529deef676193aeb7
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2019-02-01 20:04:46 -0500

    launch final update to github

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,27 @@
+const form = document.getElementById("add-form")
+const submit = document.getElementById("send")
+let btnIds = null
+
+submit.addEventListener('click', e => {
+  post();
+  console.log("submit")
+  form.reset();
+});
+let btns = document.querySelectorAll('.addBtn')
+btns.forEach( button => {
+  button.addEventListener('click', evt => {
+    console.log("check")
+    const btn = evt.currentTarget;
+    btnIds = btn.id
+  })
+})
 function openPage(pageName, elmnt, color) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tabcontent")
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("tablink")
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].style.backgroundColor = "#3a7d9e";
     }
@@ -13,5 +30,34 @@ function openPage(pageName, elmnt, color) {
     elmnt.style.backgroundColor = color;
   }
   
-  document.getElementById("defaultOpen").click();
+  document.getElementById("defaultOpen").click()
 
+function post(){
+  console.log("post")
+ 
+  const post = {
+    photo : document.getElementById('image-file').files[0].path,  
+    description: document.getElementById('fish-name').value, 
+    size: document.getElementById("fish-weight").value,
+    listId: Number(btnIds)
+}
+  let listadd = null;
+  if(btnIds == 1) {
+      listadd = 'catch-list';
+      console.log("1")
+  }
+  else if (btnIds == 2){
+      listadd = 'trip-list';
+  }
+  else {
+      listadd = 'tackle-list';
+  }
+
+  let itemId = document.getElementById(listadd)
+  const newItem = `<div class="col">
+                      <img src="${post.photo}">
+                      <h4>${post.description}</h3>
+                      <p>${post.size}</p>
+                  </div>`;
+  itemId.insertAdjacentHTML("beforeend", newItem, 'error')
+}
